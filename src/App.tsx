ad7e76b9e92e4cc2d0b55008594b279564624cf7
IPierrot123 <gbaskinridley@gmail.com>
2022-07-03 16:03:01 +0100

    Removed unused component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { FetchData, DisplayData } from './components/FetchData';
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Settings from './pages/Settings';
 import QuestionScreen from './pages/QuestionScreen';
