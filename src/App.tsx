16516303bf3177d9a1232d61d5e869a484ed91c6
IPierrot123 <gbaskinridley@gmail.com>
2022-07-03 13:48:46 +0100

    Added BrowserRouter to the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { FetchData, DisplayData } from './components/FetchData';
-
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Settings from './pages/Settings';
+import QuestionScreen from './pages/QuestionScreen';
+import ResultScreen from './pages/ResultScreen';
 function App() {
   FetchData();
 
   return (
-    <div className="App">
-      <FetchData/>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Settings />
+        </Route>
+        <Route path="/questions">
+          <QuestionScreen />
+        </Route>
+        <Route path="/results">
+          <ResultScreen />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
