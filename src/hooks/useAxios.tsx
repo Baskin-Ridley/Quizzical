9bc947d257459547a77ff3db3e7544355c10b2c5
IPierrot123 <gbaskinridley@gmail.com>
2022-07-03 16:02:30 +0100

    Created custom hook

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,37 @@
+//@ts-nocheck
+//Currently has an error on url which is why @ts-nocheck is used
+
+import React from 'react'
+import axios from 'axios'
+
+// defualt url may need to be updated to https://opentdb.com/api.php test first then change later if not working
+
+axios.defaults.baseURL = 'https://opentdb.com/'
+
+export const useAxios = ({ url }) => {
+    const [response, setResponse] = React.useState(null)
+    const [error, setError] = React.useState("")
+    const [loading, setLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        const fetchData = () => {
+            axios.get(url)
+                .then(response => {
+                    setResponse(response.data)
+                    setLoading(false)
+                })
+                .catch(error => {
+                    setError(error)
+                    setLoading(false)
+                }
+                )
+        }
+        fetchData();
+    }, [url])
+    
+  return (
+    <h1>temp</h1>
+  )
+}
+
+
