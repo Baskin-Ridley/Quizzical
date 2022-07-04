0d7169e5a068db818204c8f4a65f8dbe3f431c2a
IPierrot123 <gbaskinridley@gmail.com>
2022-07-04 11:41:20 +0100

    Created a redux reducer

---
+++ src/redux/reducer.tsx
@@ -0,0 +1,10 @@
+cosnt initialState = {
+    question_category: "",
+    question_type: "",
+    question_difficulty: "",
+    amount_of_questions: 5,
+    score: 0
+}
+
+const reducer = (state = initialState, action) => {
+}
