5d6a96d1d68ffbede659521a0bd462132ce90ffb
IPierrot123 <gbaskinridley@gmail.com>
2022-07-04 11:36:55 +0100

    Created an axios hook to get the questions in the questionscreen component

--- src/pages/QuestionScreen.tsx
+++ src/pages/QuestionScreen.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useAxios } from '../hooks/useAxios';
 
 const QuestionScreen = () => {
+
+    let apiUrl = "/api.php?amount=10"
+    const { response, loading } = useAxios ({ url: apiUrl });
+    console.log(response, loading)
+    
   return (
     <div>QuestionScreen</div>
   )
